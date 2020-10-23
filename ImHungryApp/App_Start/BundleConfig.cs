29c7a3a730dd3caf9c72941eb078a16182e208a0
PerezDC <61435556+PerezDC@users.noreply.github.com>
2020-10-22 22:03:22 -0700

    added bootstrap styling

---
+++ ImHungryApp/App_Start/BundleConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ImHungryApp
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/sass").Include(
+                      "~/dist/css/styles.css"));
+        }
+    }
+}
---
+++ ImHungryApplication/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+using System.Web.Optimization;
+using BundleTransformer.Core.Builders;
+using BundleTransformer.Core.Bundles;
+using BundleTransformer.Core.Orderers;
+using BundleTransformer.Core.Resolvers;
+
+namespace ImHungryApplication
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            var nullBulider = new NullBuilder();
+            var nullOrderer = new NullOrderer();
+
+            BundleResolver.Current = new CustomBundleResolver();
+            var commonStyleBundle = new CustomStyleBundle("~/Bundle/sass");
+
+            commonStyleBundle.Include("~/css/main.scss");
+            commonStyleBundle.Orderer = nullOrderer;
+            bundles.Add(commonStyleBundle);
+        }
+    }
+}
